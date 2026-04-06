a9fe9e83c90d2275f46931f6a1326a8018c0c66e
chinmaya <moharanachinmay300@gmail.com>
2026-04-06 12:48:42 +0530

    commit

--- backend copy/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import MySQLdb  # noqa: F401
-except ImportError:
-    try:
-        import pymysql
-    except ImportError:
-        pymysql = None
-    else:
-        pymysql.install_as_MySQLdb()
